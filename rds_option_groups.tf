18f9262a35c45e08ce944e04f1489dbe64f77630
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-06 11:21:46 +0000

    Updates values

--- rds_option_groups.tf
+++ rds_option_groups.tf
@@ -17,18 +17,20 @@ resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_a" {
     option_name = "NATIVE_NETWORK_ENCRYPTION"
 
     option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
       name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
       value = "SHA1,MD5"
     }
     option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
       name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
       value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
-    }option_settings {
-      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
-      value = "REQUESTED"
-    }option_settings {
-      name  = "SQLNET.CRYPTO_ENCRYPTION_SERVER"
-      value = "REQUESTED"
     }
   }
 }
@@ -52,18 +54,20 @@ resource "aws_db_option_group" "rds_options_oracle_se2_v12_1_a" {
     option_name = "NATIVE_NETWORK_ENCRYPTION"
 
     option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
       name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
       value = "SHA1,MD5"
     }
     option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
       name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
       value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
-    }option_settings {
-      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
-      value = "REQUESTED"
-    }option_settings {
-      name  = "SQLNET.CRYPTO_ENCRYPTION_SERVER"
-      value = "REQUESTED"
     }
   }
 }
