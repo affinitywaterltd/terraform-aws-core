c1185bebbaa626f0e4f3a23c2646a1841ae008c3
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-06 12:28:28 +0000

    Added Oracle SE2 v12.2

--- rds_option_groups.tf
+++ rds_option_groups.tf
@@ -2,7 +2,7 @@
 # Default Option Group for Oracle-SE1
 resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_a" {
   name                     = "rds-options-oracle-se1-v11-2-a"
-  option_group_description = "Default + Statpack + NW Encryption"
+  option_group_description = "Default + Statspack + NW Encryption"
   engine_name              = "oracle-se1"
   major_engine_version     = "11.2"
 
@@ -39,7 +39,7 @@ resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_a" {
 # Default Option Group for Oracle-SE2
 resource "aws_db_option_group" "rds_options_oracle_se2_v12_1_a" {
   name                     = "rds-options-oracle-se2-v12-1-a"
-  option_group_description = "Default + Statpack + NW Encryption"
+  option_group_description = "Default + Statspack + NW Encryption"
   engine_name              = "oracle-se2"
   major_engine_version     = "12.1"
 
@@ -47,7 +47,44 @@ resource "aws_db_option_group" "rds_options_oracle_se2_v12_1_a" {
 
   option {
     option_name = "STATSPACK"
+    
+  }
 
+  option {
+    option_name = "NATIVE_NETWORK_ENCRYPTION"
+
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
+      value = "SHA1,MD5"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
+      value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
+    }
+  }
+}
+
+
+# Default Option Group for Oracle-SE2
+resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_a" {
+  name                     = "rds-options-oracle-se2-v12-2-a"
+  option_group_description = "Default + Statspack + NW Encryption"
+  engine_name              = "oracle-se2"
+  major_engine_version     = "12.2"
+
+  tags = "${local.common_tags}"
+
+  option {
+    option_name = "STATSPACK"
+    
   }
 
   option {
