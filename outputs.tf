379063017b13e40c3d7517062c24c5717968bf7b
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-07-15 11:56:50 +0100

    Remove output

--- outputs.tf
+++ outputs.tf
@@ -13,9 +13,3 @@ output "rds_options_oracle_se2_v12_2_a" {
   description = "Oracle-SE2 - Default + Statpack + NW Encryption"
   value       = "${aws_db_option_group.rds_options_oracle_se2_v12_2_a.name}"
 }
-
-
-output "rds-parameter-group-oracle-se2-12-1-maximo" {
-  description = "rds-parameter-group-oracle-se2-12-1-maximo"
-  value       = "${aws_db_parameter_group.maximo.name}"
-}
