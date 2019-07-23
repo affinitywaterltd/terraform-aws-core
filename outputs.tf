0ed673ca9ac7af332ec90b2afa2f1d72f5e2aaf4
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-07-23 15:39:31 +0100

    Add RDS Option Sets

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,35 @@ output "rds_options_oracle_se2_v12_2_a" {
   description = "Oracle-SE2 - Default + Statpack + NW Encryption"
   value       = "${aws_db_option_group.rds_options_oracle_se2_v12_2_a.name}"
 }
+
+# Option sets with Timezone
+output "rds_options_oracle_se1_v11_2_b" {
+  description = "Oracle-SE1 - Default + Statpack + NW Encryption + Timezone"
+  value       = "${aws_db_option_group.rds_options_oracle_se1_v11_2_b.name}"
+}
+
+output "rds_options_oracle_se2_v12_1_b" {
+  description = "Oracle-SE2 - Default + Statpack + NW Encryption + Timezone"
+  value       = "${aws_db_option_group.rds_options_oracle_se2_v12_1_b.name}"
+}
+
+output "rds_options_oracle_se2_v12_2_b" {
+  description = "Oracle-SE2 - Default + Statpack + NW Encryption + Timezone"
+  value       = "${aws_db_option_group.rds_options_oracle_se2_v12_2_b.name}"
+}
+
+# Option sets without NW Encryption
+output "rds_options_oracle_se1_v11_2_c" {
+  description = "Oracle-SE1 - Default + Statpack + Timezone"
+  value       = "${aws_db_option_group.rds_options_oracle_se1_v11_2_c.name}"
+}
+
+output "rds_options_oracle_se2_v12_1_c" {
+  description = "Oracle-SE2 - Default + Statpack + Timezone"
+  value       = "${aws_db_option_group.rds_options_oracle_se2_v12_1_c.name}"
+}
+
+output "rds_options_oracle_se2_v12_2_c" {
+  description = "Oracle-SE2 - Default + Statpack + Timezone"
+  value       = "${aws_db_option_group.rds_options_oracle_se2_v12_2_c.name}"
+}
--- rds_option_groups.tf
+++ rds_option_groups.tf
@@ -10,7 +10,6 @@ resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_a" {
 
   option {
     option_name = "STATSPACK"
-
   }
 
   option {
@@ -47,7 +46,6 @@ resource "aws_db_option_group" "rds_options_oracle_se2_v12_1_a" {
 
   option {
     option_name = "STATSPACK"
-    
   }
 
   option {
@@ -84,7 +82,6 @@ resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_a" {
 
   option {
     option_name = "STATSPACK"
-    
   }
 
   option {
@@ -105,6 +102,213 @@ resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_a" {
     option_settings {
       name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
       value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
+    }
+  }
+}
+
+
+# Default Option Group with Timezone for Oracle-SE1
+resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_b" {
+  name                     = "rds-options-oracle-se1-v11-2-b"
+  option_group_description = "Default + Statspack + NW Encryption + Timezone"
+  engine_name              = "oracle-se1"
+  major_engine_version     = "11.2"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "NATIVE_NETWORK_ENCRYPTION"
+
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
+      value = "SHA1,MD5"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
+      value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
+    }
+  }
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Europe/London"
+    }
+  }
+}
+
+
+# Default Option Group with Timezone for Oracle-SE2
+resource "aws_db_option_group" "rds_options_oracle_se2_v12_1_b" {
+  name                     = "rds-options-oracle-se2-v12-1-b"
+  option_group_description = "Default + Statspack + NW Encryption + Timezone"
+  engine_name              = "oracle-se2"
+  major_engine_version     = "12.1"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "NATIVE_NETWORK_ENCRYPTION"
+
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
+      value = "SHA1,MD5"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
+      value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
+    }
+  }
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Europe/London"
+    }
+  }
+}
+
+
+# Default Option Group with Timezone for Oracle-SE2
+resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_b" {
+  name                     = "rds-options-oracle-se2-v12-2-b"
+  option_group_description = "Default + Statspack + NW Encryption + Timezone"
+  engine_name              = "oracle-se2"
+  major_engine_version     = "12.2"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "NATIVE_NETWORK_ENCRYPTION"
+
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
+      value = "SHA1,MD5"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
+      value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
+    }
+  }
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Europe/London"
+    }
+  }
+}
+
+
+# Default Option Group with Timezone for Oracle-SE1
+resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_c" {
+  name                     = "rds-options-oracle-se1-v11-2-c"
+  option_group_description = "Default + Statspack + Timezone"
+  engine_name              = "oracle-se1"
+  major_engine_version     = "11.2"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Europe/London"
+    }
+  }
+}
+
+
+# Default Option Group with Timezone for Oracle-SE2
+resource "aws_db_option_group" "rds_options_oracle_se2_v12_1_c" {
+  name                     = "rds-options-oracle-se2-v12-1-c"
+  option_group_description = "Default + Statspack + Timezone"
+  engine_name              = "oracle-se2"
+  major_engine_version     = "12.1"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Europe/London"
+    }
+  }
+}
+
+
+# Default Option Group with Timezone for Oracle-SE2
+resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_c" {
+  name                     = "rds-options-oracle-se2-v12-2-c"
+  option_group_description = "Default + Statspack + Timezone"
+  engine_name              = "oracle-se2"
+  major_engine_version     = "12.2"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "Timezone"
+
+    option_settings {
+      name  = "TIME_ZONE"
+      value = "Europe/London"
     }
   }
 }
