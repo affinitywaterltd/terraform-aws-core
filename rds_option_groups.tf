89a3ef72864aa8e174e6b64c14ee1683a34e74e4
De Coning <Travis.DeConing@affinitywater.co.uk>
2019-10-01 12:02:22 +0100

    add RDS output group for Oracle 12_2_b

--- rds_option_groups.tf
+++ rds_option_groups.tf
@@ -106,6 +106,40 @@ resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_a" {
   }
 }
 
+resource "aws_db_option_group" "rds_options_oracle_se2_v12_2_b" {
+  name                     = "rds-options-oracle-se2-v12-2-b"
+  option_group_description = "Default + Statspack + NW Encryption"
+  engine_name              = "oracle-se2"
+  major_engine_version     = "12.2"
+
+  tags = "${local.base_tags}"
+
+  option {
+    option_name = "STATSPACK"
+  }
+
+  option {
+    option_name = "NATIVE_NETWORK_ENCRYPTION"
+
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
+      value = "SHA1,MD5"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_SERVER"
+      value = "REQUESTED"
+    }
+    option_settings {
+      name  = "SQLNET.ENCRYPTION_TYPES_SERVER"
+      value = "RC4_256,AES256,AES192,3DES168,RC4_128,AES128,3DES112,RC4_56,DES,RC4_40,DES40"
+    }
+  }
+}
+
 
 # Default Option Group with Timezone for Oracle-SE1
 resource "aws_db_option_group" "rds_options_oracle_se1_v11_2_b" {
