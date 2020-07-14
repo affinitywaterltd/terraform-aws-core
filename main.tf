a99738b166e8039c707ae09325c0553eb7c2ca4c
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-07-14 08:45:49 +0100

    Strengthen Password Policy

--- main.tf
+++ main.tf
@@ -8,12 +8,13 @@ resource "aws_iam_saml_provider" "AzureAd" {
 # Password Policy
 
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 8
+  minimum_password_length        = 14
   require_lowercase_characters   = true
   require_numbers                = true
   require_uppercase_characters   = true
   require_symbols                = true
   allow_users_to_change_password = true
+  password_reuse_prevention      = 24
 }
 
 # Cloudtrail
