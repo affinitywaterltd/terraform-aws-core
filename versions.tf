7d79fd034ab31c86a8c08c350e5149d3b112cf14
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-12-08 08:00:16 +0000

    Add comment to provider override

--- versions.tf
+++ versions.tf
@@ -4,6 +4,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      # v3.20 breaks validation with AWS Backups - https://github.com/hashicorp/terraform-provider-aws/issues/16583
       version = "~> 3.0,!= 3.20"
     }
   }
