c6c6527877c283e1fc7ccfb438091756c80bf631
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-12-08 07:54:10 +0000

    Reversion provider version

--- versions.tf
+++ versions.tf
@@ -2,3 +2,8 @@
 terraform {
   required_version = ">= 0.12"
 }
+
+provider "aws" {
+  # Pinned due to https://github.com/hashicorp/terraform-provider-aws/issues/16583
+  version = "~> 3.0,!= 3.20"
+}
