e7a5d89194e30bc55251ccfa8de114ec3ac86cdd
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-01-26 13:43:59 +0000

    Add SNS configs

--- variables.tf
+++ variables.tf
@@ -8,6 +8,10 @@ data "terraform_remote_state" "logs" {
   }
 }
 
+data "aws_iam_role" "delivery_status_iam_role" {
+  name = "awl-sns-delivery-logging"
+}
+
 locals {
   cloudtrail_bucket_id = data.terraform_remote_state.logs.outputs.cloudtrail_bucket_id
 }
@@ -15,3 +19,30 @@ locals {
 variable "alias" {
 }
 
+variable "configure_sns" {
+  default = false
+}
+
+variable "monthly_spend_limit" {
+  default = 50
+}
+
+variable "delivery_status_iam_role_arn" {
+  default = null
+}
+
+variable "delivery_status_success_sampling_rate" {
+  default = 100
+}
+
+variable "default_sender_id" {
+  default = null
+}
+
+variable "default_sms_type" {
+  default = "Transactional"
+}
+
+variable "usage_report_s3_bucket" {
+  default = null
+}
