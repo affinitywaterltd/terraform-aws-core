4995fcc6429f85b6aab4a799e29c5eeb20e11db1
tldc22 <travis.deconing@affinitywater.co.uk>
2021-02-24 15:31:56 +0000

    update WAF Module

--- variables.tf
+++ variables.tf
@@ -53,7 +53,6 @@ variable "usage_report_s3_bucket" {
 variable "name_prefix" {
   type        = string
   description = "Name prefix used to create resources."
-  default     = "AWL-WAF-"
 }
 
 variable "ip_rate_based_rule" {
