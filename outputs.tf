508d07a087c2393bfe2245001f5604fc33d16a91
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-05-13 12:11:25 +0100

    Remove RDS outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "rds_options_oracle_se1_v11_2_a" {
-  description = "Oracle-SE1 - Default + Statpack + NW Encryption"
-  value       = aws_db_option_group.rds_options_oracle_se1_v11_2_a.name
-}
-
 output "rds_options_oracle_se2_v12_1_a" {
   description = "Oracle-SE2 - Default + Statpack + NW Encryption"
   value       = aws_db_option_group.rds_options_oracle_se2_v12_1_a.name
@@ -14,11 +9,6 @@ output "rds_options_oracle_se2_v12_2_a" {
 }
 
 # Option sets with Timezone
-output "rds_options_oracle_se1_v11_2_b" {
-  description = "Oracle-SE1 - Default + Statpack + NW Encryption + Timezone"
-  value       = aws_db_option_group.rds_options_oracle_se1_v11_2_b.name
-}
-
 output "rds_options_oracle_se2_v12_1_b" {
   description = "Oracle-SE2 - Default + Statpack + NW Encryption + Timezone"
   value       = aws_db_option_group.rds_options_oracle_se2_v12_1_b.name
@@ -40,10 +30,7 @@ output "rds_options_oracle_se2_v19_0_b" {
 }
 
 # Option sets without NW Encryption
-output "rds_options_oracle_se1_v11_2_c" {
-  description = "Oracle-SE1 - Default + Statpack + Timezone"
-  value       = aws_db_option_group.rds_options_oracle_se1_v11_2_c.name
-}
+
 
 output "rds_options_oracle_se2_v12_1_c" {
   description = "Oracle-SE2 - Default + Statpack + Timezone"
