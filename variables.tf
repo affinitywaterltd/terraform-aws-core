f8137433749295b923f9ea3f5aae6f5e2baebe85
tldc22 <travis.deconing@affinitywater.co.uk>
2021-06-09 13:52:02 +0100

    add SizeRestrictions_BODY exclusion

--- variables.tf
+++ variables.tf
@@ -175,7 +175,7 @@ locals {
       managed_rule_group_statement = {
         name        = "AWSManagedRulesCommonRuleSet"
         vendor_name = "AWS"
-        excluded_rule = []
+        excluded_rule = ["SizeRestrictions_BODY"]
       }
     },
     {
